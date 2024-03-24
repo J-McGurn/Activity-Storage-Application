948f428123b5a3e41f0fa3d3720da0f805b1750c
J-McGurn <jamie.ryan.mcgurn@gmail.com>
2024-03-24 03:53:13 +0000

    Added Movie input form

--- Application/src/Movies.java
+++ Application/src/Movies.java
@@ -6,20 +6,22 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.*;
 
 public class Movies {
     private JFrame frame;
-    private JButton input, back;
-    private JPanel buttonp, inputp, inputButton;
-    private JComboBox rating;
-    private JCheckBox genres;
-    private JTextField title;
+    private JButton input, back, confirm;
+    private JPanel backButton, inputButton, formPanel;
+    private JComboBox ratingList;
+    private JCheckBox genreList;
+    private JTextField titleField;
+    private JLabel titleLabel, ratingLabel, genreLabel;
 
     public Movies() {
         swingWindow();
         buttons();
-
     }
 
     public void swingWindow() {
@@ -27,6 +29,7 @@ public class Movies {
         frame.setTitle("Movies");
         frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
+        frame.setLayout(new FlowLayout());
         frame.setVisible(true);
     }
 
@@ -36,10 +39,17 @@ public class Movies {
         inputButton.add(input);
         frame.add(inputButton);
 
-        buttonp = new JPanel();
+        input.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                entryForm();
+            }
+        });
+
+        backButton = new JPanel();
         back = new JButton("Back");
-        buttonp.add(back);
-        frame.add(buttonp);
+        backButton.add(back);
+        frame.add(backButton);
 
         back.addActionListener(new ActionListener() {
             @Override
@@ -48,6 +58,48 @@ public class Movies {
                 new HomePage();
             }
         });
+    }
+
+    public void entryForm() {
+        formPanel = new JPanel();
+        formPanel = new JPanel(new GridLayout(0, 4));
+        // Add title
+        titleLabel = new JLabel("Title:");
+        titleField = new JTextField(20);
+        formPanel.add(titleLabel);
+        formPanel.add(titleField);
+
+        // Add star ratings
+        ratingLabel = new JLabel("Rating:");
+        String[] ratings = { "5 Star", "4 Star", "3 Star", "2 Star", "1 Star" };
+        ratingList = new JComboBox<>(ratings);
+        formPanel.add(ratingLabel);
+        formPanel.add(ratingList);
+
+        // Add genres
+        genreLabel = new JLabel("Genre(s):");
+        formPanel.add(genreLabel);
+        String[] genres = { "Action", "Adventure", "Animation", "Comedy", "Crime",
+                "Drama", "Fantasy", "Historical", "Horror", "Musical",
+                "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western" };
+        for (String genre : genres) {
+            genreList = new JCheckBox(genre);
+            formPanel.add(genreList);
+        }
+
+        // add confirm button
+        confirm = new JButton("Confirm Entry");
+        formPanel.add(confirm);
+
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+
+            }
+        });
+
+        frame.add(formPanel);
+        frame.revalidate();
     }
 
 }
