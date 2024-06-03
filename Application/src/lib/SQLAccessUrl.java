533f614dea6e0aae8746b255f7b82703c4e87a78
J-McGurn <jamie.ryan.mcgurn@gmail.com>
2024-06-03 18:02:11 +0100

    bug fix

--- Application/src/lib/SQLAccessUrl.java
+++ Application/src/lib/SQLAccessUrl.java
@@ -3,7 +3,7 @@ package lib;
 // replace ****** with root password
 public class SQLAccessUrl {
     public String url() {
-        String url = "jdbc:mysql://localhost:3306/activity_storage?user=root&password=******!&allowPublicKeyRetrieval=true&useSSL=false";
+        String url = "jdbc:mysql://localhost:3306/activity_storage?user=root&password=******&allowPublicKeyRetrieval=true&useSSL=false";
         return url;
     }
 }
